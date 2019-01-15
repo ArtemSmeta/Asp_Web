7b7605ac255ad8c43784d6a7a61a23c0583867ec
ArtemSmeta <43440709+ArtemSmeta@users.noreply.github.com>
2019-01-15 21:49:19 +0300

    Add files via upload

---
+++ ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiTest.Controllers
+{
+    //[Route("api/[controller]")]
+    [Produces("application/json")]
+    [Route("api/Values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost("SomeController")]
+        //[HttpPost]
+        public IActionResult SomeController([FromBody] Class1 nameParametr)
+        {
+            if (nameParametr == null)
+            {
+                return BadRequest();
+            }
+            Console.WriteLine("Hello_Postman");
+            return Ok(nameParametr);
+        }
+    }    
+}
